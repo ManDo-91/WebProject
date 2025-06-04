6c16ec041e8d3931fdfc447bdfae9dc050e6845d
ManDo-91 <mohamed2308673@miuegypt.edu.eg>
2025-06-04 18:32:08 +0300

    Delete signup and login.js

--- signup and login.js
+++
@@ -1,6 +0,0 @@
-    document.querySelector(".cancelbtn").addEventListener("click", function () {
-        document.querySelector("form").style.display = "none";
-        });
-    document.querySelector(".close").addEventListener("click", function () {
-        document.querySelector("form").style.display = "none";
-        });
