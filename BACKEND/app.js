b60a01b119da8f0267ece66caac5dcf67cafd60f
Zeyadshedeed <zeyad2308238@miuegypt.edu.eg>
2025-06-13 17:59:52 +0300

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,51 +1,47 @@
-const express = require('express');
+const express= require('express');
 const app = express();
+const bodyParser= require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
-const authJwt = require('./helpers/jwt');
-const errorHandler = require('./helpers/error-handler');
-
 
 app.use(cors());
-app.options('*', cors())
+app.options('*', cors());
+
+const categoriesRoutes = require('./routers/categories');
+const productsRouter=require('./routers/products');
+const usersRoutes = require('./routers/users');
+const ordersRoutes = require('./routers/orders');
+
+
+const api= process.env.API_URL;
 
 //middleware
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(morgan('tiny'));
-app.use(authJwt());
-app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
-app.use(errorHandler);
 
-//Routes
-const categoriesRoutes = require('./routes/categories');
-const productsRoutes = require('./routes/products');
-const usersRoutes = require('./routes/users');
-const ordersRoutes = require('./routes/orders');
-
-const api = process.env.API_URL;
-
+//routers
 app.use(`${api}/categories`, categoriesRoutes);
-app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/products`,productsRouter);
 app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
-//Database
-mongoose.connect(process.env.CONNECTION_STRING, {
+
+
+mongoose.connect(process.env.Connection_String/*,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'eshop-database'
-})
+    dbName: 'Scentify-database'
+}*/)
 .then(()=>{
-    console.log('Database Connection is ready...')
+    console.log('Database connection is ready...')
 })
-.catch((err)=> {
+.catch((err)=>{
     console.log(err);
 })
 
-//Server
 app.listen(3000, ()=>{
-
+    console.log(api);
     console.log('server is running http://localhost:3000');
 })
