146c78e95f2fc700b72b55a29f7104a9ed820815
Zeyadshedeed <zeyad2308238@miuegypt.edu.eg>
2025-06-16 22:58:04 +0300

    Add files via upload

---
+++ BACKEND/src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    email: {
+        service: 'gmail',
+        auth: {
+            user: 'mohamedkhalil3136@gmail.com',
+            pass: 'wuxzyzjkvllltwsg'
+        },
+        fromName: 'Scentify'
+    }
+};
